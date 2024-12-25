c9993b4dc6225054079473c301b978ede42b2a9a
AminAhmadiAsl <a.ahmadi@whiteboard-studio.nl>
2024-12-25 12:11:07 +0330

    Create Simple Application Layer

---
+++ src/Utravs.Task1.Application/IRepository/IBookingRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utravs.Task1.Domain.Domain;
+
+namespace Utravs.Task1.Application.IRepository
+{
+    public interface IBookingRepository
+    {
+        Task<int> BookAsync(int flightNumber, int passengerId);
+        Task<List<Booking>> GetAsync(int flightId);
+    }
+}
---
+++ src/Utravs.Task1.Application/IRepository/IFlightRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gridify;
+using Utravs.Task1.Domain.Domain;
+
+namespace Utravs.Task1.Application.IRepository
+{
+    public interface IFlightRepository
+    {
+        Task<int> CreateAsync(Flight flight, CancellationToken cancellationToken);
+        Task<Gridify.Paging<Flight>> GetAsync(GridifyQuery gQuery, CancellationToken cancellationToken);
+        Task UpdateSeatAsync(int flightNumber, int seatsCount, CancellationToken cancellationToken);
+    }
+}
